c3c116c3e2083383f45371c2d4839aaebe150ed1
karthikreddy8 <74849340+karthikreddy8@users.noreply.github.com>
2020-12-17 08:54:52 +0530

    initial

---
+++ app.js
@@ -0,0 +1,5 @@
+var btn = document.querySelector("#btn");
+var  expenseType = document.querySelector("#expenseType");
+ var expenseAmount = document.querySelector("#amount");
+var container = document.querySelector("#container");
+var totalExpense = document.querySelector("#totalExpense");
